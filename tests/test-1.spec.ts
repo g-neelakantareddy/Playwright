bd6f34e27977cf1f457be1a6d2363c65e1232c54
neela <gneelakantareddy143@gmail.com>
2025-12-09 08:51:46 +0530

    edited few lines -improvements

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,13 +3,14 @@ import { test, expect } from '@playwright/test';
 test('A/B Testing', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/');
   await page.getByRole('link', { name: 'A/B Testing' }).click();
-  await page.getByText('Also known as split testing.').click();
+  await expect(page.getByRole('heading', {name: 'A/B Test Variation 1'})).toBeVisible()
   await page.goBack({waitUntil : "domcontentloaded"});
 });
 test("Add/Remove Element", async ({page}) => {
   await page.goto('https://the-internet.herokuapp.com/');
   await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
   await page.getByRole('button', { name: 'Add Element' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toBeVisible()
   await page.getByRole('button', { name: 'Delete' }).click();
   await page.goBack({waitUntil : "domcontentloaded"});
 });
